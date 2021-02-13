1a377a205046ada008bec52f3f2259681a07ab08
Anwesh43 <anweshthecool0@gmail.com>
2021-02-13 11:02:51 +0530

    adding CircleParterView class

--- circleparterview/src/main/java/com/example/circleparterview/CircleParterView.kt
+++ circleparterview/src/main/java/com/example/circleparterview/CircleParterView.kt
@@ -60,3 +60,19 @@ fun Canvas.drawCPNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawCircleParter(scale, w, h, paint)
 }
+
+class CircleParterView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
