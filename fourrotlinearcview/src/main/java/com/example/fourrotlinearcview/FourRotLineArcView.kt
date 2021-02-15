2e4946d1e300bff875481f13913a38f56efdf953
Anwesh43 <anweshthecool0@gmail.com>
2021-02-15 10:38:01 +0530

    adding FRLNode class

--- fourrotlinearcview/src/main/java/com/example/fourrotlinearcview/FourRotLineArcView.kt
+++ fourrotlinearcview/src/main/java/com/example/fourrotlinearcview/FourRotLineArcView.kt
@@ -125,4 +125,45 @@ class FourRotLineArcView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class FRLNode(var i : Int, val state : State = State()) {
+
+        private var next : FRLNode? = null
+        private var prev : FRLNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = FRLNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawFRLANode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : FRLNode {
+            var curr : FRLNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
