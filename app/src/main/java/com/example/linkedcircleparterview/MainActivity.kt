74a68453ba7e901bcce757215fa56d330b84d17f
Anwesh43 <anweshthecool0@gmail.com>
2021-02-15 11:17:02 +0530

    adding View in activity

--- app/src/main/java/com/example/linkedcircleparterview/MainActivity.kt
+++ app/src/main/java/com/example/linkedcircleparterview/MainActivity.kt
@@ -4,11 +4,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
 import com.example.circleparterview.CircleParterView
+import com.example.fourrotlinearcview.FourRotLineArcView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        CircleParterView.create(this)
+        FourRotLineArcView.create(this)
         fullScreen()
     }
 }
